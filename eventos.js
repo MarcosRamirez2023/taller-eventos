b5db7fc09b69a245900692812b25fc6797045bcf
MarcosRamirez2023 <138153412+MarcosRamirez2023@users.noreply.github.com>
2023-08-07 16:59:48 -0300

    Agregado manejador de evento para el div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const divSaludo = document.querySelector('div');
+    divSaludo.addEventListener('click', function() {
+      alert('Hola! Soy el div');
+    });
+  });
+  
