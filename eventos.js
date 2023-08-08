3626602a5bdc9885c0f02abf2c5bdf6618030a7d
MarcosRamirez2023 <138153412+MarcosRamirez2023@users.noreply.github.com>
2023-08-08 17:22:43 -0300

    eventos.js

--- eventos.js
+++ eventos.js
@@ -3,4 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
   divSaludo.addEventListener('click', function() {
     alert('Hola! Soy el div');
   });
+
+  const btnSaludar = document.querySelector('#saludarBtn');
+  btnSaludar.addEventListener('click', function(event) {
+    alert('Hola!');
+    event.stopPropagation();
+  });
 });
